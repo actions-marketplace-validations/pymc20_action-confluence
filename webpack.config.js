bf8caa8927fd1c1bc2890ea5626a9ebc049a55bb
pymc20 <pymc20@naver.com>
2022-12-27 12:05:30 +0900

    수정

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
-const { resolve } = require('path')
+const {resolve} = require('path');
 
 module.exports = {
-    entry: './lib/main.js',
-    target: 'node',
-    output: {
-        filename: 'index.js',
-        path: resolve(__dirname, 'dist')
-    }
-}
+  entry: './lib/main.js',
+  target: 'node',
+  output: {
+    filename: 'index.js',
+    path: resolve(__dirname, 'dist'),
+  },
+};
