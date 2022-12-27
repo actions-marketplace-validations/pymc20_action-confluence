301bd07e8b9cab165865461c302b01593ba1a4fd
pymc20 <pymc20@naver.com>
2022-12-27 17:48:00 +0900

    수정

--- src/api.ts
+++ src/api.ts
@@ -68,7 +68,7 @@ export async function getChildrenByPage(pageId: string) {
       },
     }
   );
-  core.debug(`page: ${JSON.stringify(page)}`);
+  core.debug(`page: ${_.toString(page)}`);
   return _.get(page, 'children.page.results', []);
 }
 
