639fd5767d5097b6dc0aa05ff4da5e8d210938a9
pymc20 <pymc20@naver.com>
2022-12-28 12:13:32 +0900

    수정

--- src/make.ts
+++ src/make.ts
@@ -6,34 +6,30 @@ function makeHtml(jsonString: JSON, prevContents: string) {
   let html = '';
   if (_.isEmpty(prevContents)) {
     Object.keys(jsonString).map((x: any) => {
-      html += `<h1>${x
-        .replace('&', '&amp;')
-        .replace('<', '&lt;')
-        .replace('>', '&gt;')}</h1>`;
+      html += `<h1>${_.escape(x)}</h1>`;
       const val = _.get(jsonString, x, '');
       const splitVal = _.split(val, '\n');
       if (_.isArray(splitVal)) {
         splitVal.map(y => {
-          html += `<p>${y
-            .replace('&', '&amp;')
-            .replace('<', '&lt;')
-            .replace('>', '&gt;')}</p>`;
+          html += `<p>${_.escape(y)}</p>`;
         });
       } else {
-        html += `<p>${val
-          .replace('&', '&amp;')
-          .replace('<', '&lt;')
-          .replace('>', '&gt;')}</p>`;
+        html += `<p>${_.escape(val)}</p>`;
       }
     });
   } else {
-    const parseHtml = parse(prevContents);
-    parseHtml.childNodes.forEach(n => {
-      n.textContent = n.textContent
-        .replace('&', '&amp;')
-        .replace('<', '&lt;')
-        .replace('>', '&gt;');
+    let idx = 0;
+    const htmlKeyVal: any = {};
+    while (prevContents.match(/<[a-z/0-9]+>/)) {
+      htmlKeyVal[`::${idx}::`] = (prevContents.match(/<[a-z/0-9]+>/) || [])[0];
+      prevContents = prevContents.replace(/<[a-z/0-9]+>/, `::${idx}::`);
+      idx += 1;
+    }
+    prevContents = _.escape(prevContents);
+    Object.keys(htmlKeyVal).forEach(x => {
+      prevContents = prevContents.replace(x, htmlKeyVal[x]);
     });
+    const parseHtml = parse(prevContents);
     const titleNodeList = parseHtml.querySelectorAll('h1');
     Object.keys(jsonString).map((x: any) => {
       const val = _.get(jsonString, x, '');
@@ -44,16 +40,10 @@ function makeHtml(jsonString: JSON, prevContents: string) {
       const splitVal = _.split(val, '\n');
       if (_.isArray(splitVal)) {
         splitVal.map(y => {
-          html += `<p>${y
-            .replace('&', '&amp;')
-            .replace('<', '&lt;')
-            .replace('>', '&gt;')}</p>`;
+          html += `<p>${_.escape(y)}</p>`;
         });
       } else {
-        html += `<p>${val
-          .replace('&', '&amp;')
-          .replace('<', '&lt;')
-          .replace('>', '&gt;')}</p>`;
+        html += `<p>${_.escape(val)}</p>`;
       }
       if (titleIdx !== -1 && titleIdx + 1 < titleNodeList.length) {
         titleNodeList[titleIdx + 1].insertAdjacentHTML('beforebegin', html);
